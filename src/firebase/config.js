3d99b6b0a4b5a43a4b0bf908d3d995d1b7aacd3b
Aisha Ramiro <aishamontealto@gmail.com>
2022-12-12 18:32:40 -0300

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqWD7iQOCuRC6yafVRRWYZgvmr6X_MMwc",
+  authDomain: "miniblog-c5f90.firebaseapp.com",
+  projectId: "miniblog-c5f90",
+  storageBucket: "miniblog-c5f90.appspot.com",
+  messagingSenderId: "255082655669",
+  appId: "1:255082655669:web:4a22b92b5778949c8e33fc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db }
