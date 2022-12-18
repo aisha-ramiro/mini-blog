f26b37c8c6f24b282a09ac057f12035f028e7884
Aisha Ramiro <aishamontealto@gmail.com>
2022-12-18 18:28:19 -0300

    link for authenticated users

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 import './App.css';
 
 function App() {
 
   const [user, setUser] = useState(undefined)
-  const {auth} = useAuthentication()
+  const { auth } = useAuthentication()
 
-  const loadingUser = user === undefined 
+  const loadingUser = user === undefined
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -26,26 +28,27 @@ function App() {
     })
   }, [auth])
 
-  if(loadingUser){
+  if (loadingUser) {
     return <p>Carregando...</p>
   }
 
   return (
     <div className="App">
-      <AuthProvider value={{user}}>
-      <BrowserRouter>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/about' element={<About />} />
-          
-        </Routes>
-      </div>
-      <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/post/create' element={<CreatePost />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,49 @@
 import { NavLink } from "react-router-dom"
 
+import { useAuthentication } from "../hooks/useAuthentication"
+import { useAuthValue } from "../context/AuthContext"
+
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
+
+  const { user } = useAuthValue()
+
   return (
-   
-      <nav className={styles.navbar}>
-        <NavLink className={styles.brand} to='/'>
-          <h2>Mini<span>Blog</span></h2>
-          </NavLink>
-          <ul className={styles.links_list}>
-            <li>
-              <NavLink className={({isActive}) => (isActive ? styles.active : "")} to='/'>Home</NavLink>
-            </li>
-            <li>
-              <NavLink className={({isActive}) => (isActive ? styles.active : "")} to='/login'>Login</NavLink>
-            </li>
-            <li>
-              <NavLink className={({isActive}) => (isActive ? styles.active : "")} to='/register'>Cadastro</NavLink>
-            </li>
-            <li>
-              <NavLink className={({isActive}) => (isActive ? styles.active : "")} to='/about'>Sobre</NavLink>
-            </li>
-          </ul>
-        
 
-      </nav>
-
+    <nav className={styles.navbar}>
+      <NavLink className={styles.brand} to='/'>
+        <h2>Mini<span>Blog</span></h2>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? styles.active : "")} to='/'>Home</NavLink>
+        </li>
+        {user && (
+          <>
+            <li>
+              <NavLink className={({ isActive }) => (isActive ? styles.active : "")} to='/post/create'>Novo Post</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => (isActive ? styles.active : "")} to='/dashboard'>DashBoard</NavLink>
+            </li>
+          </>
+        )}
+        {!user && (
+          <>
+            <li>
+              <NavLink className={({ isActive }) => (isActive ? styles.active : "")} to='/login'>Login</NavLink>
+            </li>
+            <li>
+              <NavLink className={({ isActive }) => (isActive ? styles.active : "")} to='/register'>Cadastro</NavLink>
+            </li>
+          </>
+        )}
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? styles.active : "")} to='/about'>Sobre</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
