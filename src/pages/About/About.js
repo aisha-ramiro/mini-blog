f4fb083fbd46d3f89f8b7268c4324bddd1ab8797
Aisha Ramiro <aishamontealto@gmail.com>
2022-12-19 21:36:26 -0300

    add about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './About.module.css'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>Sobre o Mini<span>Blog</span></h2>
+      <p>Este projeto consiste em um blog feito com React no Front-End e Firebase no Back-End</p>
+      <Link to='/post/create' className='btn'>Criar Post</Link>
+      
     </div>
   )
 }
