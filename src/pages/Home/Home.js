83c8f51d5a338c383955413dae2e7bd6c02ab361
Aisha Ramiro <aishamontealto@gmail.com>
2023-01-03 21:37:00 -0300

    home structure

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useNavigate, Link } from "react-router-dom"
+import { useState } from "react"
+
+import styles from './Home.module.css'
 
 const Home = () => {
+
+  const [query, setQuery] = useState("")
+  const [posts, setPosts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text" placeholder="Ou busque por tags..."  onChange={(e) => setQuery(e.target.value)}/>
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="post/create" className="btn"> Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
