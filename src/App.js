2b64c33840fae03d74a711462deba6e12d3a1b1f
Aisha Ramiro <aishamontealto@gmail.com>
2023-01-06 18:44:43 -0300

    search route

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
 
 import './App.css';
 
@@ -45,6 +46,7 @@ function App() {
               <Route path='/about' element={<About />} />
               <Route path='/post/create' element={user ? <CreatePost /> : <Navigate to='login' />} />
               <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to='login' />} />
+              <Route path='/search' element={user ? <Search /> : <Navigate to='login' />} />
             </Routes>
           </div>
           <Footer />
