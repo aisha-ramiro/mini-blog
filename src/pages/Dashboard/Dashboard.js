30d3db460e71c939e82261a4d0e02d65b47e9ce1
Aisha Ramiro <aishamontealto@gmail.com>
2023-01-26 11:29:21 -0300

    dashboard structure

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,28 @@
+import { Link } from 'react-router-dom'
+
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
 import styles from './Dashboard.module.css'
 
 const Dashboard = () => {
+  const { user } = useAuthValue()
+  const uid = user.uid
+  const posts = []
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus projetos</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.nopost}>
+          <p>Não foram encontrados posts</p>
+          <Link to='/post/create' className='btn'>Criar primeiro post</Link>
+        </div>
+      ) : (
+        <div>
+        </div>
+      )}
     </div>
   )
 }
