0061d135a3ccf932fb4924838a0f623d0a7ee136
Aisha Ramiro <aishamontealto@gmail.com>
2023-03-28 21:02:00 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { auth } from 'firebase/auth';
